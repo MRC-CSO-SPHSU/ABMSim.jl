b9fb59eb548e807fa1bdb1307aae6e87fc349224
Atiyah <a.m.g.elsheikh@gmail.com>
2022-06-28 09:54:41 +0100

    Simulation types

--- MultiAgents.jl
+++ MultiAgents.jl
@@ -8,4 +8,16 @@ module MultiAgents
     include("src/abms/ABM.jl")
     include("src/abms/MultiABM.jl")
 
+    """
+    Generic interface specifying main functions for
+    executing an ABM / MABM simulation. 
+    """
+
+    include("src/simulations/AbstractSimulation.jl")
+    include("src/simulations/AbstractABMSimulation.jl")
+    include("src/simulations/ABMSimulation.jl")
+
+    include("src/simulations/AbstractMABMSimulation.jl")
+    include("src/simulations/MABMSimulation.jl") 
+
 end 
