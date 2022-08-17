1a63f26a2cf94b3a4d25e6a6092c9f0b700494cf
Atiyah <a.m.g.elsheikh@gmail.com>
2022-08-17 15:05:57 +0100

    verifying an elementary ABM

--- tests/RunTests.jl
+++ tests/RunTests.jl
@@ -14,6 +14,8 @@ using Test
 using MultiAgents: AbstractXAgent 
 using MultiAgents: initMultiAgents, verifyAgentsJLContract, 
                    getIDCOUNTER
+using MultiAgents: ABM
+using MultiAgents: add_agent!, kill_agent!
                    
 
 @testset "MultiAgents Components Testing" begin
@@ -45,6 +47,24 @@ using MultiAgents: initMultiAgents, verifyAgentsJLContract,
 
     end 
 
-    nothing
+    # a dummy ABM 
+
+    population = ABM{Person}()
+    add_agent!(population,person1)
+    add_agent!(population,person3)
+    add_agent!(population,person2)
+    add_agent!(population,person4)
+    add_agent!(person5,population)
+    add_agent!(person6,population) 
+
+    @testset verbose=true "ABM functionalities validation" begin
+
+        @test !verifyAgentsJLContract(population)
+        kill_agent!(person2,population)
+        @test verifyAgentsJLContract(population)
+
+    end 
 
 end  # testset MultiAgents components 
+
+nothing
