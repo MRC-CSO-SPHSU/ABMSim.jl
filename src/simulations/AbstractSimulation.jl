d4a2c9a33452435fbdf76a5afe2b697f8578751e
Atiyah <a.m.g.elsheikh@gmail.com>
2022-10-01 10:19:54 +0100

    using keyword arguments for simulation parameters structs

--- src/simulations/AbstractSimulation.jl
+++ src/simulations/AbstractSimulation.jl
@@ -4,6 +4,7 @@ Main specification of a Simulation type
 
 using Random
 using Mixers 
+using Parameters 
 
 using SomeUtil:   date2yearsmonths 
 
@@ -11,62 +12,63 @@ import MultiAgents: step!
 
 export stepnumber, dt, startTime, finishTime, seed 
 export initDefaultSimPars!, initDefaultFixedStepSimPars!
-export DefaultSimulation
+export AbstractSimulation, AbsFixedStepSim, DefaultSimulation
 
 abstract type AbstractSimulation end 
 
 startTime(sim::AbstractSimulation)  = sim.parameters.startTime
 finishTime(sim::AbstractSimulation) = sim.parameters.finishTime
 seed(sim::AbstractSimulation)       = sim.parameters.seed
+
+# The following could be employed (undecided)
 #example(sim::AbstractSimulation)    = sim.example 
 #time(sim::AbstractSimulation)       = sim.time  
 
-@mix struct BasicPars 
-    seed::Int 
-    startTime::Int 
-    finishTime::Int 
+@mix @with_kw struct BasicSimPars 
+    seed::Int       = 0
+    startTime::Int  = 0
+    finishTime::Int = 0 
 end # BasicPars 
 
-@BasicPars mutable struct SimPars
-    Simpars() = new(0,0,0)
-end 
+@BasicSimPars mutable struct SimPars end 
 
 "Initialize default properties"
-function initDefaultSimPars!(sim::AbstractSimulation;
-                             seed=0,startTime=0,finishTime=0) 
+function initDefaultSimPars!(sim::AbstractSimulation;startTime,finishTime,seed=0) 
     sim.parameters.seed       = seed 
     sim.parameters.startTime  = startTime
     sim.parameters.finishTime = finishTime
     # sim.time = Rational{Int}(startTime)
+    
     nothing  
 end 
 
 abstract type AbsFixedStepSim <: AbstractSimulation end
 
-dt(sim::AbsFixedStepSim)         = sim.parameters.dt
-#currstep(sim::AbsFixedStepSim)   = sim.time 
+dt(sim::AbsFixedStepSim)         = sim.parameters.dt 
 stepnumber(sim::AbsFixedStepSim) = sim.stepnumber
 
-@mix struct FixedPars 
-    dt::Rational{Int} 
+@mix @with_kw struct FixedPars 
+    dt::Rational{Int} = 0 // 1  
 end 
 
-@BasicPars @FixedPars mutable struct FixedStepSimPars
-    FixedStepSimPars(dt) = new(0,0,0,dt)
-end
+@BasicPars @FixedPars mutable struct FixedStepSimPars end
 
-function initDefaultFixedStepSimPars!(sim::AbsFixedStepSim;dt, 
-                                      seed=0,startTime=0,finishTime=0) 
+function initDefaultFixedStepSimPars!(sim::AbsFixedStepSim;dt,
+                                        startTime,finishTime,seed=0) 
 
     initDefaultSimPars!(sim;seed=seed,startTime=startTime,finishTime=finishTime)
     sim.parameters.dt   = dt
-    #sim.currstep        = Rational{Int}(startTime) 
+    sim.currstep        = Rational{Int}(startTime) 
     sim.stepnumber      = 0
 
     nothing 
 end 
 
-
+"""
+  This type corresponds to use Agents.jl capabilities for simualtion without  
+  using MultiAgents.jl, i.e. there will be no Simulation types and only Agents.jl
+  will be used for simulation  
+"""
 struct DefaultSimulation <: AbsFixedStepSim end 
 
 step!(
