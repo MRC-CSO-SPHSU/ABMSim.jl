82abc81d3d78bc1bd27a497f27a6ddd1e48100d5
Atiyah <a.m.g.elsheikh@gmail.com>
2022-12-14 08:38:35 +0000

    Making ABM parameterized type

--- src/abms/ABM.jl
+++ src/abms/ABM.jl
@@ -4,7 +4,7 @@
     probably renamed , e.g. XABM  
 """ 
 
-export ABM
+export ABMPV
 
 # dummydeclare(dict::Dict{Symbol}=Dict{Symbol}()) = nothing 
 
@@ -12,6 +12,45 @@ export ABM
 Agent based model specification for social simulations
     with data, parameters, time and variable fields 
 """
+mutable struct ABMPDV{A <: AbstractAgent, P, D, V} <: AbstractABM
+    agentsList::Vector{A}
+    
+    parameters :: P             # model parameters ideally as a struct data type
+    data       :: D            
+    variables  :: V               
+
+    ABMPDV{A,P,D,V}(agents::Vector{A},pars::P,da::D,vars::V)  where {A,P,D,V}  = 
+        new{A,P,D,V}(agents,pars,da,vars) 
+
+    #=
+    ABMPDV{A,P,D,V}(agents::Vector{A}; parameters=nothing, 
+                              data=nothing, variables=nothing) where {A,P,D,V} =
+        ABMPDV{A}(agents,parameters,data,variables)
+     
+    ABMPDV{A,P,D,V}(;parameters=nothing, 
+                data=nothing, variables=nothing,  
+                declare::Function = pars -> Vector{A}()) where {A,P,D,V}  = 
+        ABMPDV{A,P,D,V}(declare(parameters),parameters,data,variables) 
+    =# 
+end # AgentBasedModel  
+
+
+# special cases 
+
+ABMPDV{A,Nothing,Nothing,Nothing}() where A = 
+    ABMPDV{A,Nothing,Nothing,Nothing}(A[],nothing,nothing,nothing) 
+
+ABMPDV{A,Nothing,Nothing,Nothing}(agents::Vector{A}) where A = 
+    ABMPDV{A,Nothing,Nothing,Nothing}(agents,nothing,nothing,nothing) 
+
+ABMPDV{A,Nothing,Nothing,V}(v::V) where {A,V} =  
+    ABMPDV{A,Nothing,Nothing,V}(A[],nothing,nothing,v) 
+
+ABMPDV{A,P,Nothing,V}(p::P,v::V) where {A,P,V} =  
+    ABMPDV{A,P,Nothing,V}(A[],p,nothing,v) 
+
+
+#=
 mutable struct ABM{AgentType <: AbstractAgent} <: AbstractABM
     agentsList::Vector{AgentType}
     
@@ -19,18 +58,18 @@ mutable struct ABM{AgentType <: AbstractAgent} <: AbstractABM
     data                   
     variables                
 
-    ABM(agents::Vector{AgentType},pars,da,vars)  where AgentType  = 
+    ABM{AgentType}(agents::Vector{AgentType},pars,da,vars)  where AgentType  = 
         new{AgentType}(agents,pars,da,vars) 
 
     ABM{AgentType}(agents::Vector{AgentType}; parameters=nothing, 
                                   data=nothing, variables=nothing) where AgentType =
-        ABM(agents,parameters,data,variables)
+        ABM{AgentType}(agents,parameters,data,variables)
      
     ABM{AgentType}(;parameters=nothing, 
                     data=nothing, variables=nothing,  
                     declare::Function = pars -> Vector{AgentType}()) where AgentType  = 
-        ABM(declare(parameters),parameters,data,variables) 
+        ABM{AgentType}(declare(parameters),parameters,data,variables) 
 
 end # AgentBasedModel  
-
+=# 
 
