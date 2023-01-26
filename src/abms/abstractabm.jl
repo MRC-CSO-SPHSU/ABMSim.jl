bdc52610811497fd6380a079b8dde1200f867f64
Atiyah <a.m.g.elsheikh@gmail.com>
2023-01-26 15:10:09 +0000

    integration of AbstractSpace type

--- src/abms/abstractabm.jl
+++ src/abms/abstractabm.jl
@@ -1,5 +1,5 @@
 """
-Specification of an abstract ABM type as a supertype for all 
+Specification of an abstract ABM type as a supertype for all
     (elementary) Agent based models. It resembles the ABM concept
     from Agents.jl
 """
@@ -7,22 +7,25 @@ Specification of an abstract ABM type as a supertype for all
 using  MultiAgents.Util: remove_first!, remove_first_opt!
 import Random.seed!
 
-export AbstractABM 
+export AbstractABM, AbstractXSpace
 export allagents, nagents
-export add_agent!, move_agent!, 
-        kill_agent!, kill_agent_opt!, 
+export add_agent!, move_agent!,
+        kill_agent!, kill_agent_opt!,
         kill_agent_at!, kill_agent_at_opt!
 export verify_majl
 
+"Distinguished space types from Agents.jl (subject to removal if useless)"
+abstract type AbstractXSpace <: Agents.AbstractSpace end
+XSpaceTypes = Union{Nothing,AbstractXSpace}
 
 "Abstract ABM resembles the ABM concept from Agents.jl"
-abstract type AbstractABM end 
+abstract type AbstractABM end
 
 "An AbstractABM subtype to have a list of agents"
 allagents(model::AbstractABM) = model.agentsList
 
 function verify_agentsjl(model::AbstractABM)
-    #= all ids are unique =# 
+    #= all ids are unique =#
     agents = allagents(model)
     ids    = [ id for agent in agents for id = agent.id]
     return length(ids) == length(Set(ids))
@@ -31,38 +34,38 @@ end
 verify_majl(model::AbstractABM) = error("to implement")
 
 # The following part is to be seperated in an another file, to be excluded
-# when agents.jl is used 
+# when agents.jl is used
 #========================================
 Fields of an ABM
 =########################################
 
 #=
 "get a symbol property from a model"
-Base.getproperty(model::AbstractABM,property::Symbol) = 
+Base.getproperty(model::AbstractABM,property::Symbol) =
     property ∈ fieldnames(typeof(model)) ?
-        Base.getfield(model,property) : 
+        Base.getfield(model,property) :
         Base.getindex(model.properties,property)
 
 ""
-Base.setproperty!(model::AbstractABM,property::Symbol,val) = 
+Base.setproperty!(model::AbstractABM,property::Symbol,val) =
     property ∈ fieldnames(typeof(model)) ?
-        Base.setfield!(model,property,val) : 
+        Base.setfield!(model,property,val) :
         model.properties[property] = val
 =#
 
-# equivalent to operator [], i.e. model[id] 
+# equivalent to operator [], i.e. model[id]
 # Agents.jl is better since there is a hash linked list
 "@return the id-th agent (Agents.jl)"
-function Base.getindex(model::AbstractABM,id::Int64) 
-    agents = allagents(model) 
+function Base.getindex(model::AbstractABM,id::Int64)
+    agents = allagents(model)
     for agent in agents
-        if agent.id == id 
+        if agent.id == id
             return agent
-        end 
-    end   
+        end
+    end
     error("index id in $model does not exist")
     return agents[0]
-end 
+end
 
 #========================================
 Functionalities for agents within an ABM
@@ -74,17 +77,17 @@ seed!(model::AbstractABM,seed) =
 
 "number of agents"
 nagents(model::AbstractABM) = length(allagents(model))
- 
-#= 
+
+#=
 Couple of other useful functions may include:
 
-randomagent(model) : a random agent 
+randomagent(model) : a random agent
 
-randomagent(model,condition) : allagents 
+randomagent(model,condition) : allagents
 
 function allids(model)    : iterator over ids
 
-=# 
+=#
 
 #========================================
 Functionalities for agents within an ABM
@@ -98,35 +101,35 @@ add_agent!(model::AbstractABM,agent) = add_agent!(agent,model)
 
 #=
 "add agent to the model"
-function add_agent!(agent,pos,model::AgentBasedModel) 
-    nothing 
+function add_agent!(agent,pos,model::AgentBasedModel)
+    nothing
 end
-=# 
+=#
 
-"to a given position (Agents.jl)" 
+"to a given position (Agents.jl)"
 move_agent!(agent,pos,model::AbstractABM) =  error("not implemented")
 
 "remove an agent"
 kill_agent!(agent,model::AbstractABM) = remove_first!(allagents(model),agent)
 
-kill_agent_opt!(agent, model::AbstractABM) = 
+kill_agent_opt!(agent, model::AbstractABM) =
     remove_first_opt!(allagents(model), agent)
 
 kill_agent!(id::Int, model::AbstractABM) = kill_agent!(model[id],model)
 
 function kill_agent_at!(id::Int,model::AbstractABM)
-    deleteat!(allagents(model),id) 
+    deleteat!(allagents(model),id)
     nothing
-end 
+end
 
-function kill_agent_at_opt!(id::Int,model::AbstractABM) 
+function kill_agent_at_opt!(id::Int,model::AbstractABM)
     agents = allagents(model)
     agents[id] = agents[length(agents)]
-    # len = length(agents) 
+    # len = length(agents)
     # (agents[id], agents[len]) = (agents[len],agents[id])
     pop!(agents) # deleteat!(agents,len)
-    nothing 
-end 
+    nothing
+end
 
 
 "symmety"
@@ -134,13 +137,12 @@ kill_agent!(model::AbstractABM,agent) = kill_agent!(agent,model)
 kill_agent_opt!(model::AbstractABM,agent) = kill_agent_opt!(agent,model)
 
 #=
-Other potential functions 
+Other potential functions
 
-genocide(model::ABM): kill all agents 
-=# 
+genocide(model::ABM): kill all agents
+=#
 
 "ensure symmetry when initializing ABMs via their declaration"
 initial_connect!(abm2::T2,
                  abm1::T1,
                  pars) where {T1 <: AbstractABM,T2 <: AbstractABM} = initial_connect!(abm1,abm2,pars)
-
