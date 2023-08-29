693d7298239d81c1be92f6400ac840f9102063d7
Atiyah <a.m.g.elsheikh@gmail.com>
2023-08-29 12:02:11 +0100

    correct acknowledgement location

--- src/Util.jl
+++ src/Util.jl
@@ -1,46 +1,46 @@
 module Util
 
-    export AbstractExample, DummyExample, DefaultExample 
+    export AbstractExample, DummyExample, DefaultExample
 
-    export remove_first!, remove_first_opt!, date2years_months, getproperty 
+    export remove_first!, remove_first_opt!, date2years_months, getproperty
 
     "A super type for all simulation examples"
-    abstract type AbstractExample end 
+    abstract type AbstractExample end
 
     "Dummy example type"
-    struct DummyExample <: AbstractExample end 
+    struct DummyExample <: AbstractExample end
 
     "Default example type"
-    struct DefaultExample <: AbstractExample end 
-
-    # Acknowledgment: Dr. Martin Hinsch for the following algorithm
+    struct DefaultExample <: AbstractExample end
 
     "remove first occurance of e in list"
     function remove_first!(list, e)
-        e ∉ list ? throw(ArgumentError("element $(e) not in $(list)")) : nothing 
-        deleteat!(list, findfirst(x -> x == e, list)) 
-        nothing 
+        e ∉ list ? throw(ArgumentError("element $(e) not in $(list)")) : nothing
+        deleteat!(list, findfirst(x -> x == e, list))
+        nothing
     end
 
-    function remove_first_opt!(list,e) 
-        idx = findfirst(x -> x == e, list) 
-        list[idx] = list[length(list)] 
+    # Acknowledgment: Dr. Martin Hinsch for the following algorithm
+    "remove first occurance of an element e in a list (optimized version)"
+    function remove_first_opt!(list,e)
+        idx = findfirst(x -> x == e, list)
+        list[idx] = list[length(list)]
         # len = length(list)
-        # (list[len], list[idx]) = (list[idx], list[len]) 
+        # (list[len], list[idx]) = (list[idx], list[len])
         pop!(list)
-        nothing 
+        nothing
     end
 
     "convert date in rational representation to (years, months) as tuple"
     function date2years_months(date::Rational{Int})
-        date < 0 ? throw(ArgumentError("Negative age")) : nothing 
-        12 % denominator(date) != 0 ? throw(ArgumentError("$(date) not in age format")) : nothing 
-        years  = trunc(Int, numerator(date) / denominator(date)) 
+        date < 0 ? throw(ArgumentError("Negative age")) : nothing
+        12 % denominator(date) != 0 ? throw(ArgumentError("$(date) not in age format")) : nothing
+        years  = trunc(Int, numerator(date) / denominator(date))
         months = trunc(Int, numerator(date) % denominator(date) * 12 / denominator(date) )
         return (years , months)
     end
 
     "Make dictionaries look like struct for symbols keys"
-    Base.getproperty(d::Dict, s::Symbol) = s ∈ fieldnames(Dict) ? getfield(d, s) : getindex(d, s) 
+    Base.getproperty(d::Dict, s::Symbol) = s ∈ fieldnames(Dict) ? getfield(d, s) : getindex(d, s)
 
-end # module Util 
+end # module Util
