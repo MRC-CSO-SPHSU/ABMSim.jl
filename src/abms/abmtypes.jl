c029035c8863b33f983f61174ec231ea01616a26
Atiyah <a.m.g.elsheikh@gmail.com>
2023-09-20 16:56:14 +0100

    Space type

--- src/abms/abmtypes.jl
+++ src/abms/abmtypes.jl
@@ -1,52 +1,54 @@
 """
-    An Agent Based Model concept based on AbstractAgent type 
-    similar to Agents.jl, but with extension. 
-""" 
+    An Agent Based Model concept based on AbstractAgent type
+    similar to Agents.jl, but with extension.
+"""
 
 export ABMPV, SimpleABM
 
 
 """
-    SimpleABM{AgentType}
+    SimpleABMS{AgentType,SpaceType}
 
- Simple ABM type with only agents as fields    
-""" 
-struct SimpleABM{A <: AbstractAgent} <: AbstractABM
+ Simple ABM type with only agents as fields
+"""
+struct SimpleABMS{A <: AbstractAgent, SpaceType} <: AbstractABM
     agentsList::Vector{A}
+    space::SpaceType
+    SimpleABMS{A}() where A = new{A,Nothing}(A[])
+    SimpleABMS{A}(agents) where A = new{A,Nothing}(agents)
+end
 
-    SimpleABM{A}() where A = new{A}(A[]) 
-    SimpleABM{A}(agents) where A = new{A}(agents)
-end 
+const SimpleABM{A}  = SimpleABMS{A}
 
 """
-    ABMPDV{A,P,D,V} 
+    ABMPDV{A,P,D,V}
 
 Agent based model specification for social simulations
-    with data, parameters and variable fields 
+    with data, parameters and variable fields
 """
 mutable struct ABMPDV{A <: AbstractAgent, P, D, V} <: AbstractABM
     agentsList::Vector{A}
-    
+
     parameters :: P             # model parameters ideally as a struct data type
-    data       :: D            
-    variables  :: V               
+    data       :: D
+    variables  :: V
 
-    ABMPDV{A,P,D,V}(agents,pars,da,vars)  where {A,P,D,V}  = 
-        new{A,P,D,V}(agents,pars,da,vars) 
+    ABMPDV{A,P,D,V}(agents,pars,da,vars)  where {A,P,D,V}  =
+        new{A,P,D,V}(agents,pars,da,vars)
 
-end # AgentBasedModel  
+end # AgentBasedModel
 
 
-# special cases 
+# special cases
 
-ABMPDV{A,Nothing,Nothing,Nothing}() where A = 
-    ABMPDV{A,Nothing,Nothing,Nothing}(A[],nothing,nothing,nothing) 
+ABMPDV{A,Nothing,Nothing,Nothing}() where A =
+    ABMPDV{A,Nothing,Nothing,Nothing}(A[],nothing,nothing,nothing)
 
-ABMPDV{A,Nothing,Nothing,Nothing}(agents::Vector{A}) where A = 
-    ABMPDV{A,Nothing,Nothing,Nothing}(agents,nothing,nothing,nothing) 
+ABMPDV{A,Nothing,Nothing,Nothing}(agents::Vector{A}) where A =
+    ABMPDV{A,Nothing,Nothing,Nothing}(agents,nothing,nothing,nothing)
 
-ABMPDV{A,Nothing,Nothing,V}(v::V) where {A,V} =  
-    ABMPDV{A,Nothing,Nothing,V}(A[],nothing,nothing,v) 
+ABMPDV{A,Nothing,Nothing,V}(v::V) where {A,V} =
+    ABMPDV{A,Nothing,Nothing,V}(A[],nothing,nothing,v)
 
-ABMPDV{A,P,Nothing,V}(p::P,v::V) where {A,P,V} =  
-    ABMPDV{A,P,Nothing,V}(A[],p,nothing,v) 
+ABMPDV{A,P,Nothing,V}(p::P,v::V) where {A,P,V} =
+    ABMPDV{A,P,Nothing,V}(A[],p,nothing,v)
